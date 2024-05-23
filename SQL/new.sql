da55455c16f749239bb0b5091f5b7cfa4084016b
Fatmaud <fatmaudemirci@gmail.com>
2024-05-24 00:01:44 +0200

    limit opr added

--- SQL/new.sql
+++ SQL/new.sql
@@ -18,3 +18,36 @@
 --SELECT * FROM Customer WHERE CustomerId IN (1,3,5,7) AND (Country= "Canada" OR Country="Brazil") OR CustomerId=45;
 
 --**LIKE OPERATORU**--
+--1--"="--anlaminda:
+-- SELECT * FROM Customer WHERE Country LIKE "USA";
+-- --2--"iceren"--anlaminda:
+-- SELECT * FROM Customer WHERE Country LIKE "%U%";
+-- --3--"ilk harfi ... olan" degeri getir:
+-- SELECT * FROM Customer WHERE Country LIKE "U%";
+-- --4--"son harfi ... olan" degeri getir:
+-- SELECT * FROM Customer WHERE Country LIKE "%A";
+-- --5-- alt tir ile kullanim. Her _ bir karakter:
+-- --?2.harfi U olan ülkeler:
+-- SELECT * FROM Customer WHERE Country LIKE "_U%";
+-- --?3.harfi A olan ülkeler:
+-- SELECT * FROM Customer WHERE Country LIKE "__A%";
+
+--**ORDER BY OPERATORU**--
+--!! defaulttu ASC=artan siralamadir
+-- SELECT * FROM Customer ORDER BY CustomerId ;
+-- SELECT * FROM Customer ORDER BY CustomerId DESC;
+-- SELECT * FROM Customer ORDER BY LastName DESC ;
+--SELECT * FROM Customer ORDER BY FirstName;
+--SELECT * FROM Customer ORDER BY FirstName,LastName; 
+--SELECT * FROM Customer WHERE CustomerId IN (14,55) ORDER BY FirstName,LastName DESC
+
+--**LIMIT OPERATORU**--
+--SELECT * FROM Customer LIMIT 10,10; -- LIMIT ten sonraki ilk sayi filtrelenecek degerin baslangic noktasini (bir sonraki ID den getirmeye baslar), 2. deger sorgulamanin kac dger icerecegini gösterir
+--SELECT * FROM Customer LIMIT 20, 5;
+
+--**COUNT OPERATORU**--
+--SELECT COUNT(*) as "toplam kisi" FROM Customer;
+--SELECT COUNT(Total) as "toplam fatura" FROM Invoice; --secilen fielddaki sayisal verileri toplar. 
+
+
+--**JOIN**--
