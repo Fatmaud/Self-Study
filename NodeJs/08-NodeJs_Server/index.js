f7634afa042569b7a4c2a21b37ce480e069d86bf
Fatmaud <fatmaudemirci@gmail.com>
2024-06-03 19:10:56 +0200

    response messages added

--- NodeJs/08-NodeJs_Server/index.js
+++ NodeJs/08-NodeJs_Server/index.js
@@ -6,10 +6,34 @@ console.log("hi");
 
 const http = require("node:http");
 
-http
-  .createServer((req, res) => {
-    // console.log(req.method);
-    // res.end("hadi ama");
-    //console.log("server is running");
-  })
-  .listen(8000);
+// const dotenv = require(dotenv);
+//dotenv.config()
+//-->kisayolu asagida
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+//http://localhost:8000 // domain
+const HOST = process.env.HOST || "127.0.0.1";
+
+//!Serverda hersey request le response arasinda olur. bu iki safha arasinda yazilan event/sart vs ifade eden herbir code blogu middleware dir.
+
+//&Response mesajlari ile dönüs:
+//  http
+//   .createServer((req, res) => {
+//     // console.log(req.method);//get
+
+//     /*
+//     "/"--home page
+//     "/FS"--Fullstack page
+//     "/DS"--Data Science page
+//     "/CW/api"-- CW api page
+//     */
+//     if (req.url == "/") {
+//       res.end("Welcome CW");
+//     } else if (req.url == "/FS") {
+//       res.end("Welcome FS");
+//     } else if (req.url == "/CW/api") {
+//       res.end("Welcome api page");
+//     } else res.end(" <h1>no page</h1>");
+//     //console.log("server is running");
+//   })
+//   .listen(PORT, () => console.log(`server runs http://${HOST}:${PORT}`));
