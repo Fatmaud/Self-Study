128178e417572a2e36b6be78fcd3d7b8dfe2a803
Fatmaud <fatmaudemirci@gmail.com>
2024-06-03 16:19:10 +0200

    env added

--- NodeJs/08-NodeJs_Server/index.js
+++ NodeJs/08-NodeJs_Server/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+console.log("hi");
+
+//*NODE SERVER*//
+
+const http = require("node:http");
+
+http
+  .createServer((req, res) => {
+    // console.log(req.method);
+    // res.end("hadi ama");
+    //console.log("server is running");
+  })
+  .listen(8000);
